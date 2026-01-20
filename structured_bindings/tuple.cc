899ecb0ee1b5f81129c5802934586ba45b13cb80
Yair Nahum <ynahum@gmail.com>
2026-01-20 23:21:45 +0200

    add tuple c++14 impl.

--- structured_bindings/tuple.cc
+++ structured_bindings/tuple.cc
@@ -6,6 +6,17 @@ using namespace std::literals;
 
 int main()
 {
+#if __cplusplus < 201703L
+	std::cout << "before C++17\n";	
+	std::tuple<int, double, std::string> numbers = make_tuple(1, 2.0, "three"s);
+	double d;
+	int i;
+	std::string str;
+
+	std::tie(i, d, str) = numbers;                             // Store all elements in variables
+	std::cout << "i = " << i << ", d = " << d << ", s = " << str << '\n';
+#else
+	std::cout << "C++17 or later is enabled\n";	
 	std::tuple numbers = make_tuple(1, 2.0, "three"s);         // std::tuple<int, double, std::string>
 
 	/*
@@ -21,4 +32,5 @@ int main()
 	auto [i, d, str] = numbers;                                // Store all elements in variables
 
 	std::cout << "i = " << i << ", d = " << d << ", s = " << str << '\n';
+#endif
 }
