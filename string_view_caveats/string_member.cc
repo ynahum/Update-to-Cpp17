4abb657f8a7943898af28d92febd23dcf483eb5c
Yair Nahum <ynahum@gmail.com>
2026-01-24 22:45:47 +0200

    play with reference struct string member lifetime

--- string_view_caveats/string_member.cc
+++ string_view_caveats/string_member.cc
@@ -4,13 +4,15 @@
 using namespace std::literals;
 
 class ref_member {
-	std::string& str;
 	public:
-	ref_member(std::string& str) : str(str) {}
+	const std::string& str;
+	ref_member(const std::string& str) : str(str) {}
 };
 
 int main() {
-	std::string str{"test"};
+	//std::string str{"A long none optimized string against short-string optimization"};
 	//ref_member rm(str);
-	ref_member rm("test"s);     // Error
+	//ref_member rm("test"s);     // Error
+	ref_member rm("A long none optimized string against short-string optimization"s);
+	std::cout << rm.str << '\n';
 }
