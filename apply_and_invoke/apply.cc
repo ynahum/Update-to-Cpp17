6e1959ba55fe0363335cf52cf10245d92999cd2e
Yair Nahum <ynahum@gmail.com>
2026-01-25 17:33:09 +0200

    apply and invoke play

--- apply_and_invoke/apply.cc
+++ apply_and_invoke/apply.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <tuple>
 #include <string>
+#include <array>
+#include <any>
+#include <functional>
 
 using namespace std::literals;
 
@@ -9,8 +12,15 @@ void func(int i, double d, std::string s)
 	std::cout << "i = " << i << ", d = " << d << ", s = " << s << '\n';
 }
 
+void func2(int i, int d, int s)
+{
+	std::cout << "i = " << i << ", d = " << d << ", s = " << s << '\n';
+}
+
 int main()
 {
-	std::tuple<int, double, std::string>  numbers(1, 2.0, "three"s);
-	std::apply(func, numbers);             // Calls func(1, 2.0, "three"s);
+	std::tuple<int, double, std::string>  numbers_tuple(1, 2.0, "three"s);
+	std::apply(func, numbers_tuple);             // Calls func(1, 2.0, "three"s);
+	std::array<int, 3> numbers{1, 2, 3};
+	std::apply(func2, numbers);             // Calls func2(1, 2, 3);
 }
