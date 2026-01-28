d07508e7dbac2c2f0224d398717e99a872631071
Yair Nahum <ynahum@gmail.com>
2026-01-28 20:35:50 +0200

    see termination on exercise when throwing exception from parallel unseq policy

--- algorithms_and_execution/ex.cc
+++ algorithms_and_execution/ex.cc
@@ -16,7 +16,7 @@ int main()
 {
 	std::vector vec{3,1,4,1,5,9};
 
-	constexpr uint32_t TEST_NUMBER{2};
+	constexpr uint32_t TEST_NUMBER{3}; // Change this value to test different scenarios
 	if constexpr (TEST_NUMBER == 0) {
 		try {
 			//std::sort(begin(vec), end(vec));
@@ -49,6 +49,22 @@ int main()
 		namespace se = std::execution;
 		std::sort(se::par_unseq, begin(vec), end(vec));
 		// print the unsorted vector
+		print_vector(vec);	
+		return 0;
+	} else if constexpr (TEST_NUMBER == 3) {
+		using namespace std::execution;
+		namespace se = std::execution;
+		try {
+			//std::sort(begin(vec), end(vec));
+			std::sort(se::par_unseq, begin(vec), end(vec), [](int a, int b) {
+				throw std::out_of_range("Oops");
+				return a > b; }
+				);
+		}
+		catch (std::exception& e) {
+			std::cout << "Caught exception: " << e.what() << '\n';
+		}
+		// print the sorted vector
 		print_vector(vec);	
 		return 0;
 	}
