07e77f48924935f1734417430a1ab008773430c3
Yair Nahum <ynahum@gmail.com>
2026-01-29 17:19:06 +0200

    ctad constructor hint

--- ctad_and_custom_types/ex.cc
+++ ctad_and_custom_types/ex.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 template <typename T1, typename T2>
 class Test {
@@ -22,10 +24,36 @@ private:
 	T1 a;
 	T2 b;
 };
+
+template <typename T>
+class MyVector {
+public:	
+	MyVector() {
+		std::cout << "Default constructor called\n";
+	}
+	template<typename Iter>
+	MyVector(Iter begin, Iter end) : data(begin, end) {
+		std::cout << "Iterator constructor called\n";
+	}
+
+	void print() const {
+		for (const auto& item : data) {
+			std::cout << item << " ";
+		}
+		std::cout << '\n';
+	}
+
+private:
+	std::vector<T> data;
+};
+
+template <typename Iter>
+MyVector(Iter, Iter) -> MyVector<typename std::iterator_traits<Iter>::value_type>;
+
 int main()
 {
 
-	constexpr uint32_t TEST_NUMBER{0}; // Change this value to test different scenarios
+	constexpr uint32_t TEST_NUMBER{1}; // Change this value to test different scenarios
 	if constexpr (TEST_NUMBER == 0) {
 		Test<int, double> test1(42, 3.14);
 		Test<std::string, std::string> test2("Hello", "World");
@@ -35,6 +63,10 @@ int main()
 		//static_assert(std::is_same_v<decltype(test3), Test<std::string, double>>);
 
 	} else if constexpr (TEST_NUMBER == 1){
+		std::vector vec{1, 2, 3, 4, 5};
+		// Equivalent to MyVector<int> MyVector2{begin(vec), end(vec)}	
+		MyVector myVector2{begin(vec), end(vec)};
+		myVector2.print();
 	}
 
 }
