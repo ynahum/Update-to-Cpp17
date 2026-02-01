0191d7118d562962fedc12d071af428a07552274
Yair Nahum <ynahum@gmail.com>
2026-02-01 14:16:41 +0200

    structured binding

--- structured_bindings/ex.cc
+++ structured_bindings/ex.cc
@@ -4,6 +4,12 @@
 
 using namespace std::literals;
 
+struct TestStruct {
+	int id;
+	std::string name;
+	double value;
+};
+
 int main()
 {
 
@@ -19,21 +25,32 @@ int main()
 	std::tie(num, text) = p1;
 	std::cout << "Number: " << num << ", Text: " << text << "\n";
 
-	std::tuple<int, double, std::string> numbers = make_tuple(1, 2.0, "three"s);
+	std::tuple<int, double, std::string> numbers = std::make_tuple(1, 2.0, "three"s);
 	double d;
 	int i;
 	std::string str;
 
 	std::tie(i, d, str) = numbers;                             // Store all elements in variables
 	std::cout << "i = " << i << ", d = " << d << ", s = " << str << '\n';
+
+	int id;
+	std::string name;
+	double value;
+	TestStruct ts{1, "Test", 3.14};
+	std::tie(id, name, value) = std::make_tuple(ts.id, ts.name, ts.value);
+	std::cout << "id = " << id << ", name = " << name << ", value = " << value << '\n';
 #else
 	std::cout << "C++17 or later is enabled\n";	
 	std::pair p1{42, "The answer"};
 	auto [num, text] = p1; // Structured binding
 	std::cout << "Number: " << num << ", Text: " << text << "\n";
-	std::tuple numbers = make_tuple(1, 2.0, "three"s);         // std::tuple<int, double, std::string>
+	std::tuple numbers = std::make_tuple(1, 2.0, "three"s);         // std::tuple<int, double, std::string>
 	auto [i, d, str] = numbers;                                // Store all elements in variables
 	std::cout << "i = " << i << ", d = " << d << ", s = " << str << '\n';
+
+	TestStruct ts{1, "Test"s, 3.14};
+	auto [id, name, value] = ts; // Structured binding
+	std::cout << "id = " << id << ", name = " << name << ", value = " << value << '\n';
 #endif
 	} else if (TEST_NUMBER == 1){
 	}
