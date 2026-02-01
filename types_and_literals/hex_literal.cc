cc9ec23458e27f568eb1f7cee17cf867776f5139
Yair Nahum <ynahum@gmail.com>
2026-02-01 14:48:32 +0200

    hex literal

--- types_and_literals/hex_literal.cc
+++ types_and_literals/hex_literal.cc
@@ -8,8 +8,12 @@ int main()
 	// Is f a hexadecimal digit or a type suffix?
 	// auto ambig = 0x2.f;                         // Error: hex floating constants require an exponent
 
-	double d = 0xA.8p2;
-	std::cout << "d: " << d << "\n";
+	double d1 = 0xA.8p2; // 42.0
+	std::cout << "d1: " << d1 << "\n";
+	double d2 = 0xA.8p1; // 21.0
+	std::cout << "d2: " << d2 << "\n";
+	double d3 = 0xA.8p0; // 10.5
+	std::cout << "d3: " << d3 << "\n";
 	
 	float f = 0xC.7Fp3;
 	std::cout << "f: " << f << "\n";
